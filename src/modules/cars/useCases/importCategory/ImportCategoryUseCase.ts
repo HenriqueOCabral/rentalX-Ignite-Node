3d2e55c30962d548b7182ac8176ded0c16170013
Henrique-Nirol <hocabral37@gmail.com>
2021-03-21 20:51:40 -0300

    AddSwagger

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -26,6 +26,7 @@ class ImportCategoryUseCase {
           description,
         });
       }).on('end', () => {
+        fs.promises.unlink(file.path);
         resolve(categories);
       }).on('error', (err) => {
         reject(err);
