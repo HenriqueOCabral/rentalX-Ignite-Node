50c153a5fdfb815906fc8c31259e027f1e26b132
Henrique-Nirol <hocabral37@gmail.com>
2021-04-08 03:52:15 -0300

    finishingTestsWithSupertest

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -17,7 +17,9 @@ export default async (host = 'database'): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      database: process.env.NODE_ENV === 'test'
+        ? 'rentx_test' : defaultOptions,
     }),
   );
 };
